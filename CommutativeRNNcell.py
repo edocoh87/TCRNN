316a945c641bb0bd328aa89c7d00b51b19b26066
edocoh87 <edocoh@gmail.com>
2019-09-07 14:04:57 +0300

    overriding the zero_state function to initialize the hidden state with -1000 (this should be more generic in the future)

--- CommutativeRNNcell.py
+++ CommutativeRNNcell.py
@@ -1,10 +1,90 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.python.keras import activations
+from tensorflow.python.eager import context
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.framework import tensor_util
 from tensorflow.python.ops import array_ops
 from tensorflow.python.ops import math_ops
+from tensorflow.python.layers import base as base_layer
+from tensorflow.python.util import nest
 
 RAND_BOUND = 1e-4
+
+def _concat(prefix, suffix, static=False):
+  """Concat that enables int, Tensor, or TensorShape values.
+  This function takes a size specification, which can be an integer, a
+  TensorShape, or a Tensor, and converts it into a concatenated Tensor
+  (if static = False) or a list of integers (if static = True).
+  Args:
+    prefix: The prefix; usually the batch size (and/or time step size).
+      (TensorShape, int, or Tensor.)
+    suffix: TensorShape, int, or Tensor.
+    static: If `True`, return a python list with possibly unknown dimensions.
+      Otherwise return a `Tensor`.
+  Returns:
+    shape: the concatenation of prefix and suffix.
+  Raises:
+    ValueError: if `suffix` is not a scalar or vector (or TensorShape).
+    ValueError: if prefix or suffix was `None` and asked for dynamic
+      Tensors out.
+  """
+  if isinstance(prefix, ops.Tensor):
+    p = prefix
+    p_static = tensor_util.constant_value(prefix)
+    if p.shape.ndims == 0:
+      p = array_ops.expand_dims(p, 0)
+    elif p.shape.ndims != 1:
+      raise ValueError("prefix tensor must be either a scalar or vector, "
+                       "but saw tensor: %s" % p)
+  else:
+    p = tensor_shape.as_shape(prefix)
+    p_static = p.as_list() if p.ndims is not None else None
+    p = (
+        constant_op.constant(p.as_list(), dtype=dtypes.int32)
+        if p.is_fully_defined() else None)
+  if isinstance(suffix, ops.Tensor):
+    s = suffix
+    s_static = tensor_util.constant_value(suffix)
+    if s.shape.ndims == 0:
+      s = array_ops.expand_dims(s, 0)
+    elif s.shape.ndims != 1:
+      raise ValueError("suffix tensor must be either a scalar or vector, "
+                       "but saw tensor: %s" % s)
+  else:
+    s = tensor_shape.as_shape(suffix)
+    s_static = s.as_list() if s.ndims is not None else None
+    s = (
+        constant_op.constant(s.as_list(), dtype=dtypes.int32)
+        if s.is_fully_defined() else None)
+
+  if static:
+    shape = tensor_shape.as_shape(p_static).concatenate(s_static)
+    shape = shape.as_list() if shape.ndims is not None else None
+  else:
+    if p is None or s is None:
+      raise ValueError("Provided a prefix or suffix of None: %s and %s" %
+                       (prefix, suffix))
+    shape = array_ops.concat((p, s), 0)
+  return shape
+
+def _zero_state_tensors(state_size, batch_size, dtype):
+  """Create tensors of zeros based on state_size, batch_size, and dtype."""
+  def get_state_shape(s):
+    """Combine s with batch_size to get a proper tensor shape."""
+    c = _concat(batch_size, s)
+    # size = array_ops.zeros(c, dtype=dtype)
+    size = tf.constant(-1000.0) * array_ops.ones(c, dtype=dtype)
+    if not context.executing_eagerly():
+      c_static = _concat(batch_size, s, static=True)
+      size.set_shape(c_static)
+    return size
+
+  return nest.map_structure(get_state_shape, state_size)
+
 
 class CommutativeRNNcell(tf.contrib.rnn.BasicRNNCell):
     def __init__(self,
@@ -163,14 +243,42 @@ class CommutativeRNNcell(tf.contrib.rnn.BasicRNNCell):
         # print('state: ', state)
         # print('concatenated: ', array_ops.concat([inputs, state], 1))
         # print('weights: ', self._kernel)
+        # _state = tf.Print(state, [state], "state: ", summarize=100)
         gate_inputs = math_ops.matmul(
             array_ops.concat([inputs, state], 1), self._kernel)
-
+        
         # gate_inputs = nn_ops.bias_add(gate_inputs, self._bias)
         gate_outputs = self._activation(gate_inputs)
         
         # this is not in the standard rnn cell and the reason we had to implement a new cell..
         
-        #output = math_ops.matmul(gate_outputs, self._kernel_out)
-        output = tf.maximum(inputs, state)
+        output = math_ops.matmul(gate_outputs, self._kernel_out)
+        # output = tf.maximum(inputs, state)
         return output, output
+
+    def zero_state(self, batch_size, dtype):
+        """Return zero-filled state tensor(s).
+        Args:
+          batch_size: int, float, or unit Tensor representing the batch size.
+          dtype: the data type to use for the state.
+        Returns:
+          If `state_size` is an int or TensorShape, then the return value is a
+          `N-D` tensor of shape `[batch_size, state_size]` filled with zeros.
+          If `state_size` is a nested list or tuple, then the return value is
+          a nested list or tuple (of the same structure) of `2-D` tensors with
+          the shapes `[batch_size, s]` for each s in `state_size`.
+        """
+        # Try to use the last cached zero_state. This is done to avoid recreating
+        # zeros, especially when eager execution is enabled.
+        state_size = self.state_size
+        # is_eager = context.executing_eagerly()
+        # if is_eager and _hasattr(self, "_last_zero_state"):
+        #     (last_state_size, last_batch_size, last_dtype,
+        #         last_output) = getattr(self, "_last_zero_state")
+        #     if (last_batch_size == batch_size and last_dtype == dtype and last_state_size == state_size):
+        #         return last_output
+        with ops.name_scope(type(self).__name__ + "ZeroState", values=[batch_size]):
+            output = _zero_state_tensors(state_size, batch_size, dtype)
+        # if is_eager:
+        #     self._last_zero_state = (state_size, batch_size, dtype, output)
+        return output
